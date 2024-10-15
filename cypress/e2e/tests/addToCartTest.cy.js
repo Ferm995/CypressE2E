30c91652b652b9c173080803d48d145e2ea6cc6d
Ferm995 <josef.morales@wundermanthompson.com>
2024-10-15 13:03:40 -0600

    adding html reports

--- cypress/e2e/tests/addToCartTest.cy.js
+++ cypress/e2e/tests/addToCartTest.cy.js
@@ -1,7 +1,7 @@
 import {homePage} from "../../pages/homePage"
 const homePageObj = new homePage()
 import testData from '../../fixtures/testData.json'
-import { it } from "node:test"
+
 
 
 
@@ -9,12 +9,15 @@ describe(' test automation', () =>{
     //"before action" this executes the login flow for user before any test witin the class
     before(()=>{
         cy.login(testData.login.username, testData.login.password)
+        
     })
+    it('Add To Cart flow', ()=>{
+        homePageObj.searchProduct(testData.product.productName)
+        homePageObj.addToCart()
+        homePageObj.verifySucessMessage().should('contain',testData.message.successMessage).and('contains', testData.product.productName);
+    
+    })
+
+   
 })
 
-it('Add To Cart flow', ()=>{
-    homePageObj.searchProduct(testData.product.productName)
-    homePageObj.addToCart()
-    homePageObj.verifySucessMessage().should('contain',testData.message.successMessage).and('contains', testData.product.productName);
-
-})
